8990cd0934333e892181c0dafe608ca697d36cd9
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-03-05 02:01:42 +0530

    polls svelte project is initialized

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+	import CreatePollForm from "./components/CreatePollForm.svelte";
+
+	//tabs
+	let items = ['Current Polls','Add New Poll'];
+	let activeItems = 'Current Polls';
+
+	const tabChange = (e) => {
+		activeItems = e.detail;
+	}
+</script>
+
+<Header />
+<main>
+	<Tabs {activeItems} {items} on:tabChange={tabChange}/>
+	{#if activeItems === 'Current Polls'}
+		<p>Poll list Component Goes Here</p>
+		{:else if activeItems === 'Add New Poll'}
+			<CreatePollForm />
+	{/if}
+</main>
+<Footer />
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,61 @@
+<header>
+    <h1>
+        <!-- <img src="\img\logoPolls.png" alt="Poll Ninja Logo"> -->
+        P
+        <span class="loader"></span>
+        LLs
+    </h1>
+</header>
+
+<style>
+    header{
+        background: #45c496;
+        padding: 20px;
+    }
+    h1{
+        color: #FFF;
+        margin: 0;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 60px;
+    }
+    /* img{
+
+        max-width: 120px;
+    } */
+    .loader {
+        display: block;
+        width: 84px;
+        height: 84px;
+        position: relative;
+    }
+
+    .loader:before , .loader:after {
+        content: "";
+        position: absolute;
+        left: 50%;
+        bottom: 0;
+        width: 64px;
+        height: 64px;
+        border-radius: 50%;
+        background: #FFF;
+        transform: translate(-50% , -100%)  scale(0);
+        animation: push_401 2s infinite linear;
+    }
+
+    .loader:after {
+        animation-delay: 1s;
+    }
+
+    @keyframes push_401 {
+    0% , 50% {
+        transform: translate(-50% , 0%)  scale(1)
+    }
+
+    100% {
+        transform: translate(-50%, -100%) scale(0)
+    }
+}
+
+</style>
